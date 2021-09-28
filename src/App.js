edef390ab566c25cfa7f06f93c26233a32c0e38c
Shahin-Mahmud98 <shahinmahmud412@gmail.com>
2021-09-29 00:32:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ButtonGroup, Button, Spinner, Col, Card, Row } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
+
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-27&sortBy=publishedAt&apiKey=4845fae46bd04c6ca85d3569860568ee')
+      .then(res => res.json())
+      .then(data => setNews(data.articles));
+  }, [])
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        news.length === 0 ? <Spinner animation="border" />
+          :
+          <Row xs={1} md={4} className="g-4">
+            {
+              news.map(nw => <News news={nw}></News>)
+            }
+          </Row>
+      }
+
+
+      {/* <Button variant="primary">Hello Pinki</Button>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner> */}
     </div>
   );
 }
